57784a81a78d7e3db80c5994cfbcf72aeeb40111
Damilola Afolabi <damilorlah@yahoo.com>
2024-08-23 20:38:23 -0400

    Initial class setup

---
+++ Project_first/Blackboard.py
@@ -0,0 +1,26 @@
+# Student management system:
+     #Manage Students
+    # Manage Courses
+    # Manage Enrollments
+
+# Setting upn the classes
+
+class Blackboard:
+    def __init__(self) -> None:
+        pass
+
+class Person:
+    pass
+
+class Student:
+    pass
+
+class Instructor:
+    pass
+
+
+class Course:
+    pass
+
+class Enrollments:
+    pass
