d3015dcb6b1982903c91da2fca0fe209e0f8c017
Damilola Afolabi <damilorlah@yahoo.com>
2024-08-28 23:41:13 -0400

    person, student and instructor works. Need the blackboard system

--- Project_first/Blackboard.py
+++ Project_first/Blackboard.py
@@ -1,53 +1,99 @@
-# Student management system:
+# Person
+
+
+# Student
+    # Attributes:
+        # Student's name
+        # Student's ID
+        # Courses
+
+# Instructor
+    # Attributes:
+        # Instructor's name
+        # Instructor's ID
+        # Courses
+
+# Course
+    # Atrributes: Instructor, students
+
+# Student management system (Blackboard):
      #Manage Students
     # Manage Courses
     # Manage Enrollments
 
-# Setting upn the classes
+# Setting up the classes
 
 class Person:
     def __init__(self, name, id_number):
         self.name = name
-        self.ID_number = id_number
+        self.id_number = id_number
+
+    def __str__(self):
+         return f"Students: {self.name}, ID:{self.id_number}"
+       
 
 class Student(Person):
-    def __init__(self, name, id_number):
-        super().__init__(name, id_number)
-        pass
+    def __init__(self, id_number, name, major):
+        super().__init__(id_number, name)
+        self.major = major
+        self.course_list = []
 
+    def __str__(self) -> str:
+         return f"Students: {self.name}, ID:{self.id_number}, Major:{self.major}"
     
-    def course_list():
-        pass
-
 
 class Instructor(Person):
-    def __init__(self, name, ID_number):
-        super().__init__(name, ID_number)
+    def __init__(self, id_number, name, department):
+        super().__init__(id_number,name)
+        self.department = department
+        self.course_list = []
     
 
-    def department():
-        pass
+    def __str__(self) -> str:
+         return f"Students: {self.name}, ID:{self.id_number}, Major:{self.department}"
 
 
 
 class Course:
     def __init__(self, course_name, course_id):
-        pass
-    
-    def enroll():
-        pass
-
-    def student_list():
+        self.course_name = course_name
+        self.course_id = course_id
+        self.enrolled_students = []
         pass
 
+    def add_student(self, student):
+        if student not in self.enrolled_students:
+            self.enrolled_students.append(student)
+            pass
+        else:
+            print("Student already entrolled")
+
+    def remove_student(self, student):
+        if student in self.enrolled_students:
+            self.enrolled_students.remove(student)
+            pass
+        else:
+            print("student not found")
+            pass
+        pass
+
+    def __str__(self) -> str:
+         return f"Students: {self.name}, ID:{self.course_id}"
+     
+ 
 class Enrollments:
-    def __init__(self):
+    def __init__(self, student, grade):
+        self.student = student
+        self.grade = grade
+        self.grade = None
         pass
 
-    def assign_grades():
+    def assign_grades(self, grade):
+        self.grade = grade
         pass
 
-    
+    def __str__(self) -> str:
+         return f"Students: {self.name}, ID:{self.course_id}, Grade: {self.grade}"
    
 
 
